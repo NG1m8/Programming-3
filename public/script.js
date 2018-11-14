7fe7edc44d8c20aff58902f85b35fbef374c3543
Your Name <you@example.com>
2018-11-14 15:58:09 +0400

    changed file locations

--- public/script.js
+++ public/script.js
@@ -1,44 +1,45 @@
 var grassArr = [];
+var season = prompt("Write any season");
 var c = 0;
-var side = 20;
+var z;
+var side = 15;
 var matrix = [
     // [1, 0, 1, 0, 1],
     // [1, 0, 0, 0, 0],
     // [0, 1, 0, 1, 0],
-    // [0, 0, 1, 0, 0],
+    // [0, 0, 4, 3, 0],
     // [1, 1, 0, 0, 0],
-    // [1, 1, 0, 0, 0],
-    // [1, 1, 0, 0, 2]
+    // [1, 1, 0, 0, 2],
+    // [1, 1, 0, 0, 0]
 ];
-var gishatichner = []
-
-var xotakerner = []
-var vorsord = []
-var erasht = []
+var gishatichner = [];
+var xotakerner = [];
+var vorsord = [];
+var mahh = [];
 function setup() {
 
-    frameRate(1000);
+    frameRate(1);
     createCanvas(matrix[0].length * side, matrix.length * side);
     background('#acacac');
     for (var y = 0; y < matrix.length; ++y) {
         for (var x = 0; x < matrix[y].length; ++x) {
             if (matrix[y][x] == 1) {
-                var gr = new Grass(x, y);
-                grassArr.push(gr);
+
+                grassArr.push(new Grass(x, y, 1));
             }
             else if (matrix[y][x] == 2) {
-                xotakerner.push(new Xotaker(x, y));
+                xotakerner.push(new Xotaker(x, y, 2));
             }
             else if (matrix[y][x] == 3) {
-                gishatichner.push(new Gishatich(x, y))
+                gishatichner.push(new Gishatich(x, y, 3)); 
 
             }
             else if (matrix[y][x] == 4) {
-                vorsord.push(new Vorsord(x, y))
+                vorsord.push(new Vorsord(x, y, 4));
 
             }
             else if (matrix[y][x] == 5) {
-                erasht.push(new Mah(x, y))
+                mahh.push(new Mah(x, y, 5));
 
             }
         }
@@ -47,39 +48,168 @@ function setup() {
 }
 
 function draw() {
-    for (var y = 0; y < matrix.length; y++) {
+    
+    z++;
+    if (z >= 0 && z < 50) {
+        season = "spring";
+        if (season == "spring" && grassArr.length == 0) {
+            var a = Math.floor(Math.random() * g);
+            var b = Math.floor(Math.random() * h);
+            var gr = new Grass(a, b, 1);
+            grassArr.push(gr);
+        }
+    }
+    else if (z >= 50 && z < 100) {
+        season = "autumn"
+    }
+    else if (z >= 100 && z < 150) {
+        season = "summer"
+    }
+    else if (z >= 150 && z <= 200) {
+        season = "winter"
+        if (z == 200) {
+            z = 0
+        }
+    }
 
-        for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                fill("green");
-                rect(x * side, y * side, side, side);
+    if (season == "winter" || season == "Winter") {
+        for (var y = 0; y < matrix.length; y++) {
+
+            for (var x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 1) {
+                    fill("white");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 0) {
+                    fill("#acacac");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 2) {
+                    fill("#ccff66");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 3) {
+                    fill("#33cccc");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 4) {
+                    fill("#999966");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 5) {
+                    fill("black");
+                    rect(x * side, y * side, side, side);
+                }
             }
-            else if (matrix[y][x] == 0) {
-                fill("#acacac");
-                rect(x * side, y * side, side, side);
+        }
+    }
+    else if (season == "spring" || season == "Spring" || season == "Summer" || season == "summer") {
+        for (var y = 0; y < matrix.length; y++) {
+
+            for (var x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 1) {
+                    fill("green");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 0) {
+                    fill("#acacac");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 2) {
+                    fill("yellow");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 3) {
+                    fill("red");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 4) {
+                    fill("brown");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 5) {
+                    fill("black");
+                    rect(x * side, y * side, side, side);
+                }
             }
-            else if (matrix[y][x] == 2) {
-                fill("yellow");
-                rect(x * side, y * side, side, side);
+        }
+    }
+    else if (season == "Autumn" || season == "Autumn" || season == "Fall" || season == "fall") {
+        for (var y = 0; y < matrix.length; y++) {
+
+            for (var x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 1) {
+                    fill("#ff9900");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 0) {
+                    fill("#acacac");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 2) {
+                    fill("yellow");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 3) {
+                    fill("red");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 4) {
+                    fill("brown");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 5) {
+                    fill("black");
+                    rect(x * side, y * side, side, side);
+                }
             }
-            else if (matrix[y][x] == 3) {
-                fill("red");
-                rect(x * side, y * side, side, side);
-            }
-            else if (matrix[y][x] == 4) {
-                fill("brown");
-                rect(x * side, y * side, side, side);
-            }
-            else if (matrix[y][x] == 5) {
-                fill("black");
-                rect(x * side, y * side, side, side);
+        }
+    }
+    else {
+        for (var y = 0; y < matrix.length; y++) {
+
+            for (var x = 0; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 1) {
+                    fill("green");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 0) {
+                    fill("#acacac");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 2) {
+                    fill("yellow");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 3) {
+                    fill("red");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 4) {
+                    fill("brown");
+                    rect(x * side, y * side, side, side);
+                }
+                else if (matrix[y][x] == 5) {
+                    fill("black");
+                    rect(x * side, y * side, side, side);
+                }
             }
         }
     }
     for (var i in grassArr) {
         grassArr[i].bazmanal();
     }
-
+    for (var i in vorsord) {
+        vorsord[i].eat();
+    }
+    for (var i in vorsord) {
+        if (vorsord[i].energy >= 18) {
+            vorsord[i].bazmanal()
+        }
+        else if (vorsord[i].energy <= 0) {
+            vorsord[i].mahanal(i)
+        }
+    }
     for (var i in gishatichner) {
         gishatichner[i].eat();
     }
@@ -104,30 +234,20 @@ function draw() {
             xotakerner[i].mahanal(i);
         }
     }
-    for (var i in vorsord) {
-        vorsord[i].eat();
+
+    for (var i in mahh) {
+        mahh[i].eat();
     }
-    for (var i in vorsord) {
-        if (vorsord[i].energy >= 18) {
-            vorsord[i].bazmanal()
+    for (var i in mahh) {
+        if (mahh[i].energy >= 18) {
+            mahh[i].bazmanal()
         }
-        else if (vorsord[i].energy <= 0) {
-            vorsord[i].mahanal(i)
-        }
-    }
-    for (var i in erasht) {
-        erasht[i].eat();
-    }
-    for (var i in erasht) {
-        if (erasht[i].energy >= 18) {
-            erasht[i].bazmanal()
-        }
-        else if (erasht[i].energy <= 0) {
-          erasht[i].mahanal(i)
+        else if (mahh[i].energy <= 0) {
+            mahh[i].mahanal(i)
         }
     }
 }
-var g = 30
+var g = 50;
 
 for (var y = 0; y < g; y++) {
     matrix.push([])
@@ -163,3 +283,32 @@ for (var i = 0; i < 30; i++) {
     var b = Math.floor(Math.random() * g)
     matrix[a][b] = 5
 }
+/*for (var y = 0; y < matrix.length; y++) {
+
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("brown");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 5) {
+                fill("black");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    } */
